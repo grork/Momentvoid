cab9ddb2a0a802b06006dfe76a9462bb280cce77
Dominic Hopton <d.hopton@codevoid.net>
2021-11-28 14:29:03 -0800

    Add, but do not consume, `JSConfetti`
    This adds JSConfetti (https://github.com/loonywizard/js-confetti) to the app.
    
    Note, it's not used at this time.
    
    Additionally change is to move the `d.ts` files into a typings folder

---
+++ web/typings/jsconfetti.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for js-confetti
+// TypeScript Version: 4.1.2
+interface IAddConfettiConfig {
+  confettiRadius?: number,
+  confettiNumber?: number,
+  confettiColors?: string[],
+  emojis?: string[],
+  emojiSize?: number,
+}
+
+declare class JSConfetti {
+  constructor();
+
+  addConfetti(confettiConfig?: IAddConfettiConfig): Promise<void>;
+}
