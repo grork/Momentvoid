696612fb53204f0d90c44b2980fbb22473a10ae0
Dominic Hopton <d.hopton@codevoid.net>
2023-12-30 21:02:29 -0800

    Create `PreferenceKey` to enable center-like alignment
    When countdowns are within the viewport, we'd like them be aligned
    along their tops *and* overall be centered. But, when placed in the
    `ScrollView`, it sizes infinitely. Which is not great. But while it's
    possible to finagle certain things to center them *individually*,
    getting them aligned is hard.
    
    So we do some _stuff_ to calculate a spacer at the top to ensure they
    remain 'centered':
    - A preference key (`AlignerKey`) to capture the largest child
    (countdown) in the whole list of countdowns
    - An Environment item to carry the calculated spacer size down to the
    children
    - Some overlay `GeometryReaders` to capture the sizes of the items to
    do the caculation

--- ios/Momentvoid.swiftpm/CountdownView.swift
+++ ios/Momentvoid.swiftpm/CountdownView.swift
@@ -1,13 +1,20 @@
 import SwiftUI
 
 struct CountdownView: View {
+    @Environment(\.topAlignmentSpacerLength) private var spacerLength
     let countdown: Countdown
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
+            Spacer(minLength: spacerLength)
             VStack(alignment: .leading, spacing: 0) {
                 Text(countdown.title)
                 ForEach(countdown.segments) { segment in
                     SegmentView(data: segment)
+                }
+            }.overlay {
+                GeometryReader { proxy in
+                    Color.clear.preference(key: AlignerKey.self, value: proxy.size.height)
                 }
             }
         }
--- ios/Momentvoid.swiftpm/CountdownsView.swift
+++ ios/Momentvoid.swiftpm/CountdownsView.swift
@@ -1,24 +1,61 @@
 import SwiftUI
 
+func deduceSpacerHeight(viewHeight: Double, contentHeight: Double) -> Double {
+    // Don't have a size, so we can't do math. If content is bigger than the view
+    // we don't want a spacer.
+    if((viewHeight <= 0.0 || contentHeight <= 0.0) || (contentHeight >= viewHeight)) {
+        return 0.0
+    }
+    return (viewHeight - contentHeight) / 2.0
+}
+
+struct AlignerKey: PreferenceKey {
+    static let defaultValue = 0.0
+    static func reduce(value: inout Double, nextValue: () -> Double) { value = max(value, nextValue()) }
+}
+
+struct TopAlignmentSpacerLength: EnvironmentKey {
+    static let defaultValue: Double = 0.0
+}
+
+extension EnvironmentValues {
+    var topAlignmentSpacerLength: Double {
+        get { self[TopAlignmentSpacerLength.self] }
+        set { self[TopAlignmentSpacerLength.self] = newValue }
+    }
+}
+
+
 struct CountdownsView: View {
+    @State var maxContentHeight = 0.0
+    @State var viewportHeight = 0.0
+    @State var horizontalCenteringSpacerLength = 0.0
+    
     let countdowns: [Countdown]
     var body: some View {
-        GeometryReader { proxy in
-            // Make things scroll horizontally
-            ScrollView(.horizontal) {
-                HStack(alignment: .bottom) {
-                    // Spacer at the front so we can have a 'centered' like scrolling vibe. note the numbers here are just 'random'
-                    Spacer(minLength: (proxy.size.width / 2) - 100)
-                    
-                    // Actual Segments
-                    ForEach(countdowns) { countdown in
-                        CountdownView(countdown: countdown)
-                    }
-                    // Spacer at the end so we can scroll the last item into the center of the scrolling region. Numbers are just 'random'
-                    Spacer(minLength: (proxy.size.width / 2) - 100)
+        // Make things scroll horizontally
+        ScrollView(.horizontal) {
+            HStack() {
+                // Spacer at the front so we can have a 'centered' like scrolling vibe. note the numbers here are just 'random'
+                Spacer(minLength: horizontalCenteringSpacerLength)
+                
+                // Actual Segments
+                ForEach(countdowns) { countdown in
+                    CountdownView(countdown: countdown)
+                        .environment(\.topAlignmentSpacerLength, deduceSpacerHeight(viewHeight: viewportHeight, contentHeight: maxContentHeight))
                 }
-                // ^-- make sure that the hstack goes to the full height of the scrollview. Without this while the scrollview itself will be full height, the actual scroll indicator will be with the height of the hstack. e.g. in the middle of the screen.
+                // Spacer at the end so we can scroll the last item into the center of the scrolling region. Numbers are just 'random'
+                Spacer(minLength: horizontalCenteringSpacerLength)
             }
+        }.overlay {
+            GeometryReader { proxy in
+                Color.clear.onChange(of: proxy.size.height, initial: true) {
+                    viewportHeight = proxy.size.height
+                }
+            }
+        }
+        .onPreferenceChange(AlignerKey.self) { value in
+            maxContentHeight = value
         }
     }
 }
