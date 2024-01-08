0de35bf89447861b705fa432bb22f34935d3ba3f
Dominic Hopton <d.hopton@codevoid.net>
2024-01-08 12:58:55 -0800

    Comment `SegmentView` + minimize `withAnimation`
    Primarily, this change comments all the things `SegmentView`. Along the
    way it slims down some of the parameters passed in the preview 'cause
    they were redundant.
    
    Additionally, due to separate learnings and not wanting to comment it,
    we no longer need the `withAnimation` on the toggle to see the
    animation when changing the segments value

--- ios/Momentvoid.swiftpm/SegmentView.swift
+++ ios/Momentvoid.swiftpm/SegmentView.swift
@@ -1,20 +1,35 @@
 import SwiftUI
-
+/**
+ * A single segment for a unit of time. Displays things in visually
+ * appealing form, and is aware of dynamic type to adjust the type
+ * size
+ */
 struct SegmentView: View {
+    /// The `Segment` data to be displayed
     var data: Segment
+    
+    /// Scaled type size for the numeric part of the segment
     @ScaledMetric(relativeTo: .largeTitle) var digitSize = 100
-    @ScaledMetric(relativeTo: .largeTitle) var spacingSize = -25
+    
+    /// Scaled kerning value for the numeric part of the segment
     @ScaledMetric(relativeTo: .body) var digitKerning = -3.0
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text(String(data.value))
+                // Using monospaced digits aligns the text so that it doesn't
+                // jiggle as values change by fixing the digit width. E.g.,
+                // 11 is narrower than 55 without this.
                 .font(.system(size: digitSize).monospacedDigit())
                 .fontWeight(.heavy)
                 .kerning(digitKerning)
                 .lineLimit(1)
                 .padding([.bottom, .top], -20)
-                .contentTransition(.numericText(countsDown: true))
+                .contentTransition(.numericText(countsDown: true)) // Animate the digit change
+                // Ensure that an animation is played on this view. Without this an animation
+                // is only performed when the state change happens within a `withAnimation`
+                // block
+                .animation(.easeInOut, value: data.value)
                 .frame(minWidth: 125, alignment: .leading)
             
             Text(data.title)
@@ -27,31 +42,43 @@ struct SegmentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
-    struct Fiddler: View {
-        @State var theSegment: Segment
-        let size: ContentSizeCategory
-        let label: String
+    /// Enable validation that the content transition animation
+    /// is working by allowing the preview to toggle the value
+    /// within the segment, and see the animation
+    struct AnimatedSegmentChangePreviewHelper: View {
+        @State var displayedSegment: Segment
+        
         var body: some View {
             VStack {
-                SegmentView(data: theSegment)
-                    .environment(\.sizeCategory, size)
-                Button("Toggle") {
-                    withAnimation {
-                        theSegment = Segment(title: label, value: Int.random(in: 1111..<9999), id: label)
-                    }
-                }
+                SegmentView(data: displayedSegment)
+                
+                Button("Randomize") {
+                    displayedSegment = Segment(
+                        title: displayedSegment.title, 
+                        value: Int.random(in: 1111..<9999),
+                        id: displayedSegment.id)
+                 }.environment(\.sizeCategory, .medium) // Force fixed size for toggle button
             }
         }
     }
     
     static var previews: some View {
+        // Simple segment that follows the natural dynamic type size. Enables
+        // the 'palette' view in Xcode to show all the dynamic sizes.
         SegmentView(data: Segment(title: "Natural", value: 7777, id: "natural"))
             .previewDisplayName("Natural")
         
+        // Enumerate all the typesizes and render each as it's own preview
+        // so that we can flip between them and make sure the layout clips
+        // truncates, etc.
         ForEach(ContentSizeCategory.allCases, id: \.hashValue) { size in
             let label = String(reflecting: size)
             let segment = Segment(title: label, value: 7777, id: label)
-            Fiddler(theSegment: segment, size: size, label: label)
+            
+            // Make sure we're using the wrapper so we can hit the toggle
+            // button and inspect the animation
+            AnimatedSegmentChangePreviewHelper(displayedSegment: segment)
+                .environment(\.sizeCategory, size)
                 .previewDisplayName(label)
         }
     }
